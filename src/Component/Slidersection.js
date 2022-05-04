2058e50a7c1e65f2595312f09a1cdbbbdf163137
akku19 <akashsen127@gmail.com>
2022-05-04 19:12:58 +0530

    add slider

--- src/Component/Slidersection.js
+++ src/Component/Slidersection.js
@@ -5,15 +5,18 @@ const Slidersection = () => {
     dots: true,
     arrows:false,
     infinite: true,
-    speed: 500
+    speed: 500,
+    autoplay:true
   };
   return (
     <div className="App">
     <Slider {...settings}>
-    <div><img src="./hero-mob.png" class="" alt='' /></div>
-    <div><img src="./hero-mob.png" class="" alt='' /></div>
-    <div><img src="./hero-mob.png" class="" alt='' /></div>
-    <div><img src="./hero-mob.png" class="" alt='' /></div>
+    <div><img src="./poster-1.png" class="" alt='' /></div>
+    <div><img src="./poster-2.png" class="" alt='' /></div>
+    <div><img src="./poster-3.png" class="" alt='' /></div>
+    <div><img src="./poster-4.png" class="" alt='' /></div>
+    <div><img src="./poster-5.png" class="" alt='' /></div>
+    <div><img src="./poster-6.png" class="" alt='' /></div>
     </Slider>
   </div>
   )
